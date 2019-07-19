90f8e9e5653619f74ff463afada621bc8ffc587c
ArathG <agonzalez.airsoft@outlook.com>
2019-07-19 12:35:27 -0500

    PRE-013
    Add footer componet with php

---
+++ views/components/footer-component.php
@@ -0,0 +1,56 @@
+<head>
+    <link rel="stylesheet" href="../source/css/footer.css">
+</head>
+
+<footer class="page-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col m4 s12">
+                <img src="../source/img/PREDEV.png" height="50" width="150">
+                <p class="font-color-gray">
+                    PREDEV Es una plataforma para la
+                    monitorización y prevención de la discriminación del estado de Veracruz.
+                </p>
+                <div class="divider"></div>
+                <div class="pt-5"><strong class="text-footer-item">UNETE A </strong>
+                    <strong class="text-footer-item font-color-green">NOSOTROS</strong></div>
+                <a class="waves-effect waves-light btn btn-register">Registrarse</a>
+                <br>
+            </div>
+
+            <div class="col m4 s12 align-center">
+                <div><img src="../source/img/AirSoft.png" height="80" width="120"></div>
+                <p class="font-color-gray">
+                    Necesitas una solución nosotros la desarrollamos.
+                </p>
+                <div class="divider"></div>
+                <div class="pt-5"><strong class="text-footer-item">CONTAC</strong>
+                <strong class="text-footer-item font-color-green ml-4">TANOS</strong></div>
+                <br>
+                <a href=""><i class="fab fa-facebook-f font-color-blue fz-20 social-icon"></i></a>
+                <a href=""><i class="fab fa-twitter font-color-blue fz-20 social-icon"></i></a>
+                <a href=""><i class="fab fa-instagram font-color-blue fz-20 social-icon"></i></a>
+            </div>
+
+            <div class="col m4 s12">
+                <div class="pt-5"><strong class="text-footer-item">DESARRO</strong>
+                <strong class="text-footer-item font-color-green ml-4">LLADORES</strong></div>
+                <br>
+                <i class="fas fa-phone font-color-blue fz-20"></i>
+                <label class="font-color-gray fz-15"> Telefono:</label>
+                <p class="s12 font-color-gray mt-2">272-224-8659</p>
+                <br>
+                <i class="fas fa-envelope font-color-blue fz-20"></i>
+                <label class="font-color-gray fz-15"> Correo electronico:</label>
+                <p class="s12 font-color-gray mt-2">airssoft.develop@outlook.com</p>
+            </div>
+
+        </div>
+    </div>
+    <div class="footer-copyright">
+        <div class="container">
+            © 2019 Copyright Todos los derechos reservados por AirSoft.
+            <a class="grey-text text-lighten-4 right" href="#!">Contactanos</a>
+        </div>
+    </div>
+</footer>
