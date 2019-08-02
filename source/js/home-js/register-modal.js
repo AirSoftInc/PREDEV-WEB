557e248ff6cc13e9e6a005b1f3ea5db8d654d1ee
EddyLopezB <elopez.airsoft@outlook.com>
2019-08-02 12:01:18 -0500

    PRE-019
    Formulario registro

--- source/js/home-js/register-modal.js
+++ source/js/home-js/register-modal.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
     $('select').material_select();
+
+    $("#userType").change(function () {
+      const userType = document.getElementById("userType").value;
+      console.log(userType);     
   });
+});
--- views/components/home-components/register-modal.php
+++ views/components/home-components/register-modal.php
@@ -4,32 +4,112 @@
 
 <div id="register-modal" class="modal modal-fixed-footer">
     <div class="modal-content">
-        <h5 class="pl-10">Información de la cuenta</h5>
-        <div class="divider"></div>
+        <h5 class="pl-10" id="divider-bold">INFORMACIÓN DE LA CUENTA</h5>
+        <div class="divider" id="divider-register"></div>
         <br>
 
         <form class="col s12">
             <div class="row">
+                <div class="row">
+                    <div class="input-field col s5">
+                        <select  id="userType" name="userType"> 
+                            <option value="" disabled selected>Selecciona tu tipo de usuario</option>
+                            <option value="I">Institución</option>
+                            <option value="U">Usuario PREDEV</option>
+                        </select>
+                        <label>Tipo de Usuario</label>
+                    </div>
 
-                <div class="input-field col s4">
-                    <select>
-                        <option value="" disabled selected>Selecciona tu tipo de usuario</option>
-                        <option value="I">Institución</option>
-                        <option value="U">Usuario PREDEV</option>
-                    </select>
-                    <label>Materialize Select</label>
+                    <div class="input-field col s7">
+                        <i class="fas fa-user prefix position-icon"></i>
+                        <input id="name" type="text" class="validate test">
+                        <label for="icon_prefix">Nombre(s)</label>
+                    </div>
                 </div>
 
-                <div class="input-field col s4">
-                    <i class="fas fa-phone-alt prefix position-icon"></i>
-                    <input id="icon_prefix" type="text" class="validate test">
-                    <label for="icon_prefix">First Name</label>
+                <div class="input-field col s6">
+                    <i class="fas fa-user prefix position-icon"></i>
+                    <input id="lastname" type="text" class="validate test">
+                    <label for="icon_prefix">Primer Apellido</label>
                 </div>
 
-                <div class="input-field col s4">
-                    <i class="fas fa-phone-alt prefix position-icon"></i>
-                    <input id="icon_prefix" type="text" class="validate test">
-                    <label for="icon_prefix">First Name</label>
+                <div class="input-field col s6">
+                    <i class="fas fa-user prefix position-icon"></i>
+                    <input id="surname" type="text" class="validate test">
+                    <label for="icon_prefix">Segundo Apellido</label>
+                </div>
+
+                <div class="row">
+                    <div class="input-field col s12">
+                        <i class="fas fa-envelope prefix position-icon"></i>
+                        <input id="email" type="text" class="validate test">
+                        <label for="icon_prefix">Correo Electrónico</label>
+                    </div>
+                </div>
+
+                <div class="col s12">
+                    <h3 style="font-size: 25px;" class="title" id="divider-bold">UBICACIÓN</h3>
+                </div>
+                <div class="divider col s12" id="divider-register"></div>
+                
+                <div class="input-field col s12">
+                    <i class="fas fa-map-marker-alt prefix position-icon"></i>
+                    <input id="address" type="text" class="validate test">
+                    <label for="icon_prefix">Dirección</label>
+                </div>
+
+                <div class="input-field col s6">
+                    <i class="fas fa-city prefix position-icon"></i>
+                    <input id="city" type="text" class="validate test">
+                    <label for="icon_prefix">Ciudad</label>
+                </div>
+
+                <div class="input-field col s6">
+                    <i class="fas fa-envelope prefix position-icon"></i>
+                    <input id="zipcode" type="text" class="validate test">
+                    <label for="icon_prefix">Código Postal</label>
+                </div>
+
+                <div class="row">
+                    <div class="input-field col s6">
+                        <i class="fas fa-city prefix position-icon"></i>
+                        <input id="municipality" type="text" class="validate test">
+                        <label for="icon_prefix">Municipio</label>
+                    </div>
+                </div>
+                <div id="aditionalInfo"></div>
+                <div class="col s12">
+                    <h3 style="font-size: 25px;" class="title" id="divider-bold">INFORMACIÓN ADICIONAL</h3>
+                </div>
+                <div class="divider col s12" id="divider-register"></div>
+
+                <div class="input-field col s6">
+                    <i class="fas fa-phone prefix position-icon"></i>
+                    <input id="telephone" type="text" class="validate test">
+                    <label for="icon_prefix">Teléfono</label>
+                </div>
+
+                <div class="input-field col s6">
+                    <i class="fab fa-facebook-square prefix position-icon"></i>
+                    <input id="facebook" type="text" class="validate test">
+                    <label for="icon_prefix">Facebook</label>
+                </div>
+
+                <div class="input-field col s6">
+                    <i class="fab fa-twitter prefix position-icon"></i>
+                    <input id="twitter" type="text" class="validate test">
+                    <label for="icon_prefix">Twitter</label>
+                </div>
+
+                <div class="input-field col s6">
+                    <i class="fab fa-battle-net prefix position-icon"></i>
+                    <input id="webpage" type="text" class="validate test">
+                    <label for="icon_prefix">Página Web</label>
+                </div>
+
+                <div class="input-field col s12">
+                    <textarea id="description" class="materialize-textarea"></textarea>
+                    <label for="textarea1">Descripción</label>
                 </div>
 
             </div>
@@ -37,7 +117,8 @@
     </div>
 
     <div class="modal-footer">
-        <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+        <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Cancelar</a>
+        <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Registrarse</a>
     </div>
 </div>
 
