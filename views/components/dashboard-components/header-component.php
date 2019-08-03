f09692bedd8b45a7626b84235a7f723f7b1f1f59
Arath Gonzalez <agonzalez.airsoft@outlook.com>
2019-08-02 22:04:02 -0500

    PRE-021
    Perfil

--- views/components/dashboard-components/header-component.php
+++ views/components/dashboard-components/header-component.php
@@ -1,14 +1,20 @@
-<head>
-    <link rel="stylesheet" href="../source/css/dashboard-css/dashboard.css">
-</head>
-
 <header>
     <nav class="top-nav bb-10 h-85">
         <div class="nav-wrapper bg-blue">
             <a class="page-title">Panel administrador</a>
-            <div class="right mr-15 profile-buttom"></div>
+            <!-- <div class="right mr-15 profile-buttom"></div> -->
+            <div class="profile-buttom">
+                <div class="fixed-action-btn horizontal click-to-toggle" style="position: absolute; right: 24px;">
+                    <a class="btn-floating btn-large red profile-buttom-size">
+                        <img src="../../source/img/profile.jpg" class="profile-img-size">
+                    </a>
+                    <ul>
+                        <li><a class="btn-floating blue"><i class="fas fa-globe-americas icon-web"></i></a></li>
+                        <li><a class="btn-floating blue"><i class="fas fa-user-cog icon-position"></i></a></li>
+                        <li><a class="btn-floating blue"><i class="fas fa-sign-out-alt icon-position"></i></a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </nav>
 </header>
-
-<script src="../source/js/jquery-3.4.1.min.js"></script>
